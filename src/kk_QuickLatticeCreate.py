7822af58946338a9b22b85dc34878c9368312654
kursadk <kursadk@live.com>
2013-05-01 19:54:41 -0500

    progress, basic lattice work with proper position and scale

--- src/kk_QuickLatticeCreate.py
+++ src/kk_QuickLatticeCreate.py
@@ -1,9 +1,11 @@
 #----------------------------------------------------------
 # File lattice.py
-
-
 #----------------------------------------------------------
+
+#TODO Calcuate the final position of objects
+
 import bpy
+import mathutils
  
 def createIcoSphere(origin):
     # Create an icosphere
@@ -24,17 +26,18 @@ def createIcoSphere(origin):
             lower.add([v.index], 0.5, 'REPLACE')
     return ob
  
-def createLattice(origin,size):
+def createLattice(size,pos):
     # Create lattice and object
     lat = bpy.data.lattices.new('MyLattice')
     ob = bpy.data.objects.new('LatticeObject', lat)
     
+    ob.scale=size
+    ob.location=pos
+    
    
-    ob.location = origin
     ob.show_x_ray = True
     # Link object to scene
     scn = bpy.context.scene
-    ob.scale=size
     scn.objects.link(ob)
     scn.objects.active = ob
     scn.update()
@@ -59,7 +62,66 @@ def createLattice(origin,size):
             #pt.co[k] = points[n][k]
             pass
     return ob
- 
+
+
+def selectedVerts(obj):
+#     vertices=bpy.context.active_object.data.vertices
+    vertices=obj.data.vertices
+    
+    selverts=[vert for vert in vertices if vert.select==True]
+    
+    print(selverts)
+    
+#    print(type(selverts[0]))    
+    return selverts
+
+
+def findBBox(obj,selvertsarray):
+    minx=0
+    miny=0
+    minz=0
+    
+    maxx=0
+    maxy=0
+    maxz=0
+    print("  ")    
+    
+    #Media Centers
+    x_sum=0
+    y_sum=0
+    z_sum=0
+    
+    for vert in selvertsarray:
+        co=vert.co*obj.matrix_world
+        x_sum+=co.x
+        y_sum+=co.y
+        z_sum+=co.z
+        
+        if co.x<minx: minx=co.x
+        if co.y<miny: miny=co.y
+        if co.z<minz: minz=co.z
+
+        if co.x>maxx: maxx=co.x
+        if co.y>maxy: maxy=co.y
+        if co.z>maxz: maxz=co.z
+        
+        print("local cord", vert.co)
+        print("world cord", co)
+
+    pos_median=[x_sum/len(selvertsarray), y_sum/len(selvertsarray), z_sum/len(selvertsarray)]            
+    print("min - max", minx," ", miny," ", minz, " ",  maxx," ", maxy," ", maxz)
+    print("median point ->",pos_median)
+
+    return [minx, miny, minz, maxx, maxy, maxz, pos_median  ]
+
+
+def latticeDelete():
+#     for ob in bpy.context.scene.objects:
+#         if "Lattice" in ob.name:
+    if "Lattice" in bpy.context.scene.objects:
+        bpy.ops.object.select_pattern(pattern="Lat*")
+        bpy.ops.object.delete(use_global=False) 
+    
 def run(origin):
     #sphere = createIcoSphere(origin)
     
@@ -72,8 +134,21 @@ def run(origin):
     
     
     #-----
+    #Delete all the lattices for testing
+    latticeDelete()
+    
     obj=bpy.context.active_object
-    lat = createLattice(origin,(2,2,2))
+    selvertsarray=selectedVerts(obj)
+    bbox=findBBox(obj,selvertsarray)
+    
+    latsize=[bbox[0]-bbox[3], bbox[1]-bbox[4], bbox[2]-bbox[5]]
+    
+    
+    size=mathutils.Vector( (abs(latsize[0]), abs(latsize[1]), abs(latsize[2])) )
+    pos=mathutils.Vector( ( bbox[6][0], bbox[6][1], bbox[6][2]) )
+    print("lattce size, pos", size, " ", pos)
+    lat = createLattice(size, pos )
+    
     
     modif=obj.modifiers.new("latticetemp","LATTICE")
     modif.object=lat
